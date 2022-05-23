e884d2d201122e022ac9930c44fe030698d47a12
Patrícia Souza <patricia.sz.br@gmail.com>
2022-05-23 15:29:44 -0300

    Innitial commit

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordList = {
+  carro: ["Motor", "Porta", "Capo", "Pneu", "Antena"],
+  fruta: ["Banana", "Maca", "Pera", "Mamao", "Laranja"],
+  corpo: ["Braco", "Perna", "Cerebro", "Pescoco", "Olhos"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Cadeira"],
+  programacao: ["Linguagem", "Framework", "Javascript", "React", "Next"],
+  alimento: ["Arroz", "Feijao", "Carne", "Leite", "Ovo"],
+};
